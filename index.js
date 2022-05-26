ff004d0cfa845e837015f265242018a9628565e0
xk.chen <xk.chen@knowyourself.cc>
2022-05-26 17:07:12 +0800

    feat: 1.0.2

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// 跟节点样式.
-const el_css = `width: 300px;margin: 0 auto;position: relative;box-sizing: border-box;overflow: hidden;`;
 // 样式引入.
 import './verification.css';
 
@@ -15,12 +13,14 @@ class verificationCode {
       shotWidth: _params.shotWidth || 200,
       shotHeight: _params.shotHeight || 200,
     };
+    // 校验时间.
+    this.durationTime = 0;
 
     // 单位换算- 300是验证码背景图展示宽度.
     this.unitSize = this.params.bgWidth / 300;
     // 根节点
     this.el = document.getElementById(el);
-    this.el.style = el_css + `width: 300px; height: ${this.params.bgHeight / this.unitSize + 36}px`;
+    this.el.style = `width: 324px;`;
     // 绑定监听事件对象。
     this.temp = {};
 
@@ -38,12 +38,25 @@ class verificationCode {
   // 初始化Dom节点
   innerHTMLElement() {
     const temp = `
-      <div class="code-background"></div>
-      <div class="code-screenshot"></div>
-      <div class="code-drag">
-        <div class="code-left"></div>
-        <input type="range" class="code-range" value="0">
-        <div class="code-again">重试</div>
+      <div class="code-cntr">
+        <div class="code-tips">拖动下方滑块完成拼图</div>
+        <div class="code-box">
+          <div class="code-background"></div>
+          <div class="code-screenshot"></div>
+          <div class="code-success">
+            <div class="code-success-icon"></div>
+            <div class="code-success-title"></div>
+          </div>
+        </div>
+        <div class="code-drag">
+            <div class="code-left"></div>
+            <div class="code-range-before"></div>
+            <input type="range" class="code-range" value="0">
+          </div>
+        <div class="code-btn-cntr">
+          <div class="code-again" title="重试"></div>
+          <div class="code-closed" title="关闭"></div>
+        </div>
       </div>
     `;
     this.el.innerHTML = temp;
@@ -54,42 +67,46 @@ class verificationCode {
       left: document.querySelector('.code-left'), // 左侧拖动进度条
       ipt: document.querySelector('.code-range'), // input range
       btn: document.querySelector('.code-again'), // 重试按钮
+      closed: document.querySelector('.code-closed'), // 重试按钮
+      success: document.querySelector('.code-success'), // 重试按钮
+      icon: document.querySelector('.code-success-icon'),
+      title: document.querySelector('.code-success-title'),
     };
-    const { box, gap, left, ipt, btn } = this.temp;
+    const { box, gap, left, ipt, btn, closed } = this.temp;
     // 获取容器的宽度.
     this.boxWidth = parseInt(window.getComputedStyle(box, null)['width']);
     //获取滑块的宽度.
     this.gapWidth = parseInt(window.getComputedStyle(gap, null)['width']);
 
-    ipt.style.height = '36px';
-    btn.style.height = '36px';
-    btn.style.lineHeight = '36px';
-    btn.style.fontSize = '12px';
-    // 绑定重定按钮
+    // 绑定关闭按钮.
+    closed.addEventListener('click', () => {
+      this.el.style.display = 'none';
+    });
+
+    // 绑定重试按钮
     btn.addEventListener('click', () => {
-      if (this.verification_code) return;
-      this.verification_code = '';
       ipt.value = 0;
       ipt.disabled = false;
-      btn.style.display = 'none';
-      gap.style.left = '0px';
-      left.style.width = '0px';
-      btn.style.width = '0px';
-      btn.innerHTML = '重试';
-      btn.style.background = '';
+      gap.style.left = '13px';
+      left.style.width = '52px';
       this.getCaptureInfo();
+      this.temp.success.style.opacity = 0;
+      this.temp.icon.style.backgroundImage = '';
     });
+
     // 图片位置实时移动
     ipt.addEventListener('input', () => {
       const width = ((this.boxWidth - this.gapWidth) * ipt.value) / 100;
-      gap.style.left = width + 'px';
+      gap.style.left = width + 12 + 'px';
       left.style.width = width + 54 + 'px';
-      btn.style.width = 300 - (width + 54) + 'px';
+      if (!this.durationTime) {
+        this.durationTime = new Date().getTime();
+        console.log(this.durationTime, 'durationTime');
+      }
     });
     // 图片拖动结束.
     ipt.addEventListener('change', () => {
       ipt.disabled = true;
-      btn.style.display = 'block';
       // 获取截图当前坐标
       var gap_ps = this.getElementPagePosition(gap);
       // 获取背景图坐标
@@ -118,11 +135,29 @@ class verificationCode {
       const { status, result } = response.data;
       if (status == 'success') {
         this.verification_code = result.code;
-        this.temp.btn.innerHTML = '成功';
+        this.closed();
       } else {
-        alert(JSON.stringify(result));
+        this.temp.success.style.opacity = 1;
+        const img =
+          'https://cdn-public.knowyourself.cc/kyapp_vest_post/2022/05/26/09c0cc1b42e74a46ae4852baf5ad5ae2---88x88.png';
+        this.temp.icon.style.backgroundImage = `url("${img}")`;
+        this.temp.title.innerHTML = JSON.stringify(result.error_msg);
       }
     });
+  }
+
+  // 关闭
+  closed() {
+    // 提示
+    this.temp.success.style.opacity = 1;
+    const curTime = new Date().getTime();
+    const duration_time = (curTime - this.durationTime) / 1000;
+    this.temp.title.innerHTML = `只用了${duration_time.toFixed(1)}秒，简直比闪电还快`;
+    // 清除节点.
+    setTimeout(() => {
+      this.el.innerHTML = '';
+      this.durationTime = 0;
+    }, 1 * 1000);
   }
 
   // 获取验证码信息
@@ -141,7 +176,7 @@ class verificationCode {
         // 验证码截图样式设置.
         this.temp.gap.style.width = Math.ceil(this.params.shotWidth / this.unitSize) + 'px';
         this.temp.gap.style.height = Math.ceil(this.params.shotHeight / this.unitSize) + 'px';
-        this.temp.gap.style.top = Math.floor(result.y / this.unitSize) + 'px';
+        this.temp.gap.style.top = Math.ceil(result.y / this.unitSize) + 'px';
       } else {
         alert('请求验证码失败～');
       }
